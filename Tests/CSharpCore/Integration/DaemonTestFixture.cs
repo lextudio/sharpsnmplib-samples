29f56afeffd8ff9c3cf3368980c89cca58427256
Lex Li <support@lextm.com>
2019-09-17 14:03:05 -0400

    Added a new IPv6 test case.

--- Tests/CSharpCore/Integration/DaemonTestFixture.cs
+++ Tests/CSharpCore/Integration/DaemonTestFixture.cs
@@ -172,6 +172,7 @@ namespace Lextm.SharpSnmpLib.Integration
                 var users1 = new UserRegistry();
                 var response = await message.GetResponseAsync(serverEndPoint, users1, socket);
                 Assert.Equal(SnmpType.ResponsePdu, response.TypeCode());
+                Assert.Equal(message.RequestId(), response.RequestId());
             }
             finally
             {
@@ -199,7 +200,37 @@ namespace Lextm.SharpSnmpLib.Integration
 
                 const int time = 1500;
                 var response = message.GetResponse(time, serverEndPoint, socket);
-                Assert.Equal(0x4bed, response.RequestId());
+                Assert.Equal(SnmpType.ResponsePdu, response.TypeCode());
+                Assert.Equal(message.RequestId(), response.RequestId());
+            }
+            finally
+            {
+                if (SnmpMessageExtension.IsRunningOnWindows)
+                {
+                    engine.Stop();
+                }
+            }
+        }
+
+        [Fact]
+        public void TestResponseIPv6()
+        {
+            var engine = CreateEngine();
+            engine.Listener.ClearBindings();
+            var serverEndPoint = new IPEndPoint(IPAddress.IPv6Loopback, Port.NextId);
+            engine.Listener.AddBinding(serverEndPoint);
+            engine.Start();
+
+            try
+            {
+                Socket socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Dgram, ProtocolType.Udp);
+                GetRequestMessage message = new GetRequestMessage(0x4bed, VersionCode.V2, new OctetString(communityPublic),
+                    new List<Variable> { new Variable(new ObjectIdentifier(oidIdentifier)) });
+
+                const int time = 1500;
+                var response = message.GetResponse(time, serverEndPoint, socket);
+                Assert.Equal(SnmpType.ResponsePdu, response.TypeCode());
+                Assert.Equal(message.RequestId(), response.RequestId());
             }
             finally
             {
