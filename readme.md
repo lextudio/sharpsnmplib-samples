46e680e81532729123f6327b3ab607eae81da123
Lex Li <support@lextm.com>
2026-02-26 19:28:36 -0500

    Drop .NET Framework support

--- readme.md
+++ readme.md
@@ -2,7 +2,9 @@
 
 # C# SNMP Library (#SNMP) Samples
 
-This is the source code repository of C# SNMP Library (#SNMP) samples. Visual Studio 2022 and .NET 8 SDK is required to compile it on Windows.
+This is the source code repository of C# SNMP Library (#SNMP) samples. Requires Visual Studio 2022 and .NET 8 SDK (or later) to compile.
+
+**Note:** Starting with v13, .NET Framework support has been dropped. The library now targets .NET Standard and .NET Core/modern .NET only (.NET 8, 9, 10, and later).
 
 The following can be found at [C# SNMP Library homepage](https://sharpsnmp.com),
 
@@ -13,6 +15,6 @@ The following can be found at [C# SNMP Library homepage](https://sharpsnmp.com),
 
 Issues can be created directly at GitHub.
 
-Questions might be posted to [StackOverflow.com](https://stackoverflow.com) with sharp-snmp tag. 
+Questions might be posted to [StackOverflow.com](https://stackoverflow.com) with sharp-snmp tag.
 
 Samples are provided as they were, and have limited support under LeXtudio commercial support contracts.
--- release.ps1
+++ release.ps1
@@ -4,42 +4,12 @@ param(
     [string] $Configuration = 'Release'
 )
 
-# Improved OS detection
-$isWindowsOS = ($PSVersionTable.PSVersion.Major -ge 6 -and $PSVersionTable.Platform -eq 'Win32NT') -or ($PSVersionTable.PSVersion.Major -lt 6 -and $env:OS -match 'Windows')
-$isMacOSX = $PSVersionTable.PSVersion.Major -ge 6 -and $PSVersionTable.OS -match 'Darwin'
-$isLinuxOS = $PSVersionTable.PSVersion.Major -ge 6 -and $PSVersionTable.OS -match 'Linux'
+$solution = "SharpSnmpLib.Samples.slnx"
+Write-Host "Using cross-platform solution: $solution"
 
-if ($isWindowsOS) {
-    Write-Host "Detected Windows OS. Use VSWhere instead."
-
-    $msbuild = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe -products * -nologo | select-object -first 1
-    if (![System.IO.File]::Exists($msBuild))
-    {
-        Write-Host "MSBuild doesn't exist. Exit."
-        exit 1
-    }
-
-    $solution = "SharpSnmpLib.Samples.Windows.slnx"
-    & $msBuild $solution /p:Configuration=$Configuration /t:restore
-    & $msBuild $solution /p:Configuration=$Configuration /t:clean
-    & $msBuild $solution /p:Configuration=$Configuration
-} else {
-    # On macOS or Linux
-    if ($isMacOSX) {
-        Write-Host "Detected macOS."
-    } elseif ($isLinuxOS) {
-        Write-Host "Detected Linux."
-    } else {
-        Write-Host "Detected non-Windows OS (unknown type)."
-    }
-    
-    $solution = "SharpSnmpLib.Samples.slnx"
-    Write-Host "Using cross-platform solution: $solution"
-    
-    & dotnet restore $solution -c $Configuration
-    & dotnet clean $solution -c $Configuration
-    & dotnet build $solution -c $Configuration
-}
+& dotnet restore $solution -c $Configuration
+& dotnet clean $solution -c $Configuration
+& dotnet build $solution -c $Configuration
 
 if ($LASTEXITCODE -ne 0)
 {
